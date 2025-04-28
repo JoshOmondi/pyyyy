41a30971549d5f29478ca6f7ab66999c4656b2d3
Joshua Omondi Ochieng <joshuaomondi3334@gmail.com>
2025-04-28 14:42:11 +0300

    add my first python project

---
+++ hello.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Creating a simple DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [24, 27, 22]}
+df = pd.DataFrame(data)
+
+# Displaying the DataFrame
+print(df)
